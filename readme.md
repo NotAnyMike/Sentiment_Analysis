2a1277b48c84f87503dc143f108d6842da45f65d
Mike Woodcock <mwc1.0@hotmail.com>
2017-05-07 13:25:57 -0500

    Adding files and correcting the error of special characters

--- readme.md
+++ readme.md
@@ -1,5 +1,12 @@
 # Sentiment Analysis Project
 
-Here will be located the respository for the Sentiment Analysis project for the CL UTL
+Here will be located the respository for the Sentiment Analysis project for the CL UTL.
 
-Don't forget to do `source sa/bin/activate` and `pip freeze > requirements.txt`
+## General information
+
+This project will use MongoDB as a database, in order to run it run `mongod --dbpath docs/db`
+
+## Useful commands
+
+* Don't forget to do `source sa/bin/activate` and `pip freeze > requirements.txt`
+* I recommend to have the MongoDB installed not only through pip, but also through the binary or brew
--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+from pymongo import MongoClient
 from utils.csv2json import SemicolonToJson
 
-print SemicolonToJson("docs/test/tuits.csv", ["date", "user", "full_name", "tweet", "id", "app", "followers", "follows", "retweets", "favorites", "verified", "user_since", "location", "bio", "profile_img", "maps"])
-#print SemicolonToJson("smallcsv.csv",["number1","number2","number3","name"])
+entries = SemicolonToJson("docs/test/tuits.csv", ["date", "user", "full_name", "tweet", "id", "app", "followers", "follows", "retweets", "favorites", "verified", "user_since", "location", "bio", "profile_img", "maps"])
+#entries =  SemicolonToJson("docs/test/smallcsv.csv",["number1","number2","number3","name"])
+
+client = MongoClient()
+db = client.test
+
+result = db.test.insert_one(entries[0])
+print result
