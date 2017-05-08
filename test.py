c71f1659a0c1e4378c19b6f92c8b50bb84957c7c
Mike Woodcock <mwc1.0@hotmail.com>
2017-05-07 19:23:24 -0500

    Adding the tockenizer

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 from pymongo import MongoClient
+from nltk.corpus import stopwords
 from utils.csv2json import SemicolonToJson
 
+stop = set(stopwords.words('spanish'))
+
+#Reading the file
 entries = SemicolonToJson("docs/test/tuits.csv", ["date", "user", "full_name", "tweet", "id", "app", "followers", "follows", "retweets", "favorites", "verified", "user_since", "location", "bio", "profile_img", "maps"])
 #entries =  SemicolonToJson("docs/test/smallcsv.csv",["number1","number2","number3","name"])
 
+#Tokenizing every tweet
+for entry in entries:
+    entry['tokens'] = [i for i in entry['tweet'].lower().split() if i not in stop]
+
+#Connecting to the db
 client = MongoClient()
 db = client.test
 
+#Saving the results in the db
 result = db.test.insert_one(entries[0])
 print result
--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
 from nltk.corpus import stopwords
 stop = set(stopwords.words('spanish'))
-t = "Vencer la corrupcion menos la dictadura de penalosa que usted apoya y proteje con su silencio"
-print [i for i in t.lower().split() if i not in stop]
+t = "Vencer la corrupción menos estarás la dictadura dé peñalosa que usted apoya y proteje con su silencio".decode('utf8')
+tokens = ([i for i in t.lower().split() if i not in stop])
+print tokens
+for x in tokens:
+    print x.encode('utf8')
