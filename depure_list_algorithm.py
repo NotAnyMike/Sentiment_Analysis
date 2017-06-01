ee31dd7fd5cee02acf0b6b4be369b3cf3c91681f
Mike Woodcock <mwc1.0@hotmail.com>
2017-06-01 11:37:01 -0500

    the depure fucntion is ready, next: work on taking care of quotes

--- depure_list_algorithm.py
+++ depure_list_algorithm.py
@@ -4,7 +4,7 @@ import csv
 import re
 
 #asking fo the name of the file
-file_location = raw_input("write the location of the csv file: ")
+file_location = input("write the location of the csv file: ")
 if file_location == "0":
     file_location = "docs/test/tuits.csv"
 
@@ -16,10 +16,10 @@ for entry in entries:
     entry["depured"] = re.sub(r'(https)([^\s]*)(\s|$)', '', entry["tweet"].lower())
 
 #save the value
-f = csv.writer(open(file_location.replace(".csv", "") + "_depure.csv", "wb+"))
+f = csv.writer(open(file_location.replace(".csv", "") + "_depured.csv", "w", encoding='utf-8-sig'), dialect='excel', delimiter=";")
 
 # Write CSV Header, If you dont need that, remove this line
 f.writerow(["date", "user", "full_name", "tweet", "id", "app", "followers", "follows", "retweets", "favorites", "verified", "user_since", "location", "bio", "profile_img", "maps", "depured"])
 
 for x in entries:
-    f.writerow([x["date"], x["user"], x["full_name"], x["tweet"].encode('utf-8'), x["id"], x["app"], x["followers"], x["follows"], x["retweets"], x["favorites"], x["verified"], x["user_since"], x["location"], x["bio"], x["profile_img"], x["maps"], x["depured"].encode('utf-8')])
+    f.writerow([x["date"], x["user"], x["full_name"], x["tweet"], x["id"], x["app"], x["followers"], x["follows"], x["retweets"], x["favorites"], x["verified"], x["user_since"], x["location"], x["bio"], x["profile_img"], x["maps"], x["depured"]])
--- utils/csv2json.py
+++ utils/csv2json.py
@@ -3,14 +3,17 @@ import json, codecs
 
 def SemicolonToJson(filename,fields):
     toReturn = []
-    with codecs.open(filename, 'r', encoding='utf-8', errors='replace') as f:
+    #with codecs.open(filename, 'r', encoding='utf-8', errors='replace') as f:
+    with open(filename,'r') as f:
         for line in f:
-            values = map(lambda s: s.strip(), line.split(","))
-            value = {}
-            for attribute in fields:
-                index = fields.index(attribute)
-                value[attribute] = values[index]
+            #values = map(lambda s: s.strip(), line.split(","))
+            if '"' not in line:
+                values = list(map(lambda s: s.strip(), line.split(",")))
+                value = {}
+                for attribute in fields:
+                    index = fields.index(attribute)
+                    value[attribute] = values[index]
 
-            toReturn.append(value)
+                toReturn.append(value)
 
     return toReturn
